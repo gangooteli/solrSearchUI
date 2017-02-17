507afce1b02b8d44c282c25544fd93528cd0baa4
Shekhar Solanki <shekhar@entronic.com>
2017-02-17 10:56:32 +0530

    initial ui

---
+++ autoCmpQuery.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){
+
+	searchString = $('input');
+	server = "http://localhost:8983/solr/";
+	fParam = "wiki/suggest?hl=on&indent=on&q=";
+	eParam = "&wt=json";
+	searchQuery="";
+	result="";
+	
+	searchString.bind("keyup", function(){
+		table = $('tbody');
+		tString = $('input').val().trim();
+		
+		if (tString.length <= 0){
+			table.empty();
+		} else {
+			tString = "list of c";//----------------
+			
+			//searchQuery = server + fParam + tString + eParam;
+			searchQuery = "file:///Users/shekhar/Dropbox/Public/nikhil/test.json";//-----------------
+			$.ajax({
+				type	:'GET',
+				//url		:'http://localhost:8983/solr/wiki/suggest',
+				url		:searchQuery,
+				crossDomain:true,
+				// data	:{
+				// 	hl : 'on',
+				// 	indent: 'on',
+				// 	q : tString,
+				// 	wt : 'json'
+
+				// },
+				dataType: 'jso',
+				success: function(result){
+						//console.log(result['suggest']['businessSuggester'][tString]["suggestions"]);
+						suggestions = result['suggest']['businessSuggester'][tString]["suggestions"];
+						//ul = $('ul');
+						table.empty();
+						for (i = 0; i < suggestions.length; i++) {
+							console.log(suggestions[i]['term']);
+							tr = $('<tr></tr>');
+							tr.append($('<td>' + suggestions[i]['term'] + '</td>'));
+							console.log(tr);
+							tr.appendTo(table);
+						}
+					//console.log(result.hasOwnProperty("suggest"));//----------------
+				}
+			});
+		}
+
+	});
+});
+
+// http://localhost:8983/solr/wiki/suggest?hl=on&indent=on&q=list%20of%20c&wt=json
+//https://shrib.com/3cBgjdVS6d7bDgT
+//103.47.153.188
